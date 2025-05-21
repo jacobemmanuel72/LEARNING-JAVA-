20873efa5bda6038bd7089542ed0b300eb5ac492
EMMANUEL JACOB <135885254+jacobemmanuel72@users.noreply.github.com>
2025-05-21 16:23:29 +0000

    public class Main {     public static void main(String[] args) {         int myAge = 25;         int herAge = 30;         System.out.println(myAge);         System.out.println(herAge);         int age = myAge + herAge;         System.out.println(age);     } }

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int myAge = 25;
+        int herAge = 30;
+        System.out.println(myAge);
+        System.out.println(herAge);
+        int age = myAge + herAge;
+        System.out.println(age);
+    }
+}
