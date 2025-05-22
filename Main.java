7aab3392117ef374a63b1b45fcc85d8d351f6bb4
EMMANUEL JACOB <135885254+jacobemmanuel72@users.noreply.github.com>
2025-05-22 18:23:03 +0000

    Implement bubble sort algorithm and display sorting process

--- Main.java
+++ Main.java
@@ -1,10 +1,35 @@
 public class Main {
+
     public static void main(String[] args) {
-        int myAge = 25;
-        int herAge = 30;
-        System.out.println(myAge);
-        System.out.println(herAge);
-        int age = myAge + herAge;
-        System.out.println(age);
+        int nums[] = {7, 2, 8, 9, 5};
+        int size = num.length;
+        int temp = 0;
+
+        System.out.println("before sorting");
+        for (int num : nums) {
+            System.out.print(nums + " ");
+        }
+
+        for (int i = 0; i < size; i++) {
+
+            for (int j = i + 1; j < size; j++) {
+
+                if (nums[i] > nums[j]) {
+
+                    temp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = temp;
+                }
+            }
+            System.out.println();
+            for (int num : nums) {
+                System.out.print(num + " ");
+            }
+        }
+        System.out.println();
+        System.out.println("after sorting");
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
     }
 }
