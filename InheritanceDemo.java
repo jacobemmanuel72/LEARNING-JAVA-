b57593ea6dff3c974b4590ad99dd7a419dd358e8
EMMANUEL JACOB <135885254+jacobemmanuel72@users.noreply.github.com>
2025-05-25 21:54:40 +0000

    class Animal {     void eat() {         System.out.println("This animal eats food.");     } }
    class Cat extends Animal {
        void meow() {
            System.out.println("The Cat Meow.");
        }
    }
    
    public class InheritanceDemo {
        public static void main(String[] args) {
            Cat myCat = new Cat();
            myCat.eat();   // Inherited method
            myCat.meow();  // Cat's own method
        }
    }

--- InheritanceDemo.java
+++ InheritanceDemo.java
@@ -4,16 +4,16 @@ class Animal {
     }
 }
 
-class Dog extends Animal {
-    void bark() {
-        System.out.println("The dog barks.");
+class Cat extends Animal {
+    void meow() {
+        System.out.println("The Cat Meow.");
     }
 }
 
 public class InheritanceDemo {
     public static void main(String[] args) {
-        Dog myDog = new Dog();
-        myDog.eat();   // Inherited method
-        myDog.bark();  // Dog's own method
+        Cat myCat = new Cat();
+        myCat.eat();   // Inherited method
+        myCat.meow();  // Cat's own method
     }
 }
